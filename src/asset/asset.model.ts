5f555e1b6990eb107f61a04bd45490108653ce33
enzotorrs <enzotorres969@gmail.com>
2026-02-13 19:42:17 -0300

    feat: add asset model

---
+++ src/asset/asset.model.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  HasMany,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'asset',
+})
+export class Asset extends Model {
+  @ApiProperty()
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false
+  })
+  folder: boolean;
+
+  @ApiProperty()
+  @ForeignKey(() => Asset)
+  @Column({
+    type: DataType.INTEGER,
+    field: 'parent_asset_id',
+    allowNull: false,
+  })
+  parentAssetId: number;
+
+  @ApiProperty()
+  @BelongsTo(() => Asset, { as: 'parentAsset' })
+  parentAsset: Asset;
+
+  @ApiProperty()
+  @HasMany(() => Asset, { as: 'childAssets' })
+  childAssets: Asset[];
+}
