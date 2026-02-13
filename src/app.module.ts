acd168268b6264f10e5c9e13e4ec432e4e561d47
enzotorrs <enzotorres969@gmail.com>
2026-02-13 19:44:18 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,33 @@
 import { Module } from '@nestjs/common';
+import { AssetModule } from './asset/asset.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadModels: true,
+        synchronize: true,
+        logging: console.log,
+      }),
+      inject: [ConfigService],
+    }),
+    AssetModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Ceos API')
+    .setVersion('developing')
+    .build();
+
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
