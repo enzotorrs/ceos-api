8a921e8c23e12c5eb7bd18ca5d444fe4e827346f
enzotorrs <enzotorres969@gmail.com>
2026-02-20 08:12:23 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   const config = new DocumentBuilder()
     .setTitle('Ceos API')
     .setVersion('developing')
+    .addBearerAuth()
     .build();
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
