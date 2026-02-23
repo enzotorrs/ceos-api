a595fb4f53e59d4dcdbb9bd3e16273c312634c33
enzotorrs <enzotorres969@gmail.com>
2026-02-23 10:07:43 -0300

    refact: remove create user endpoint
    will be replaced by signup auth endpoint

--- src/auth/user/user.controller.ts
+++ src/auth/user/user.controller.ts
@@ -1,17 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {  Controller } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDTO } from './dtos/create_user.dto';
-
-import { UserResponseDTO } from './dtos/user_response.dto';
-import { ApiDataResponse } from '../../common/decorators/api-data-response.decorator';
 
 @Controller('auth/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post()
-  @ApiDataResponse(UserResponseDTO)
-  async create(@Body() userPayload: CreateUserDTO) {
-    return this.userService.create(userPayload);
-  }
 }
